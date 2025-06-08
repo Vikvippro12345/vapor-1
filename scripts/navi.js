05cf0c3bf6f03241a488c54609144e72f79ec16c
technonyte dev <technonytealternate+1@outlook.com>
2025-06-08 14:34:51 +0000

    v3.18 New Chatroom

--- scripts/navi.js
+++ scripts/navi.js
@@ -6,8 +6,8 @@ const navItems = [
   { title: "Home", icon: "fa-home", url: "page/home.html" },
   { title: "Games", icon: "fa-gamepad-modern", url: "page/games.html" },
   { title: "VM", icon: "fa-desktop", url: "page/vm-priv.html" },
+  { title: "Chat", icon: "fa-comments-alt", url: "/chat/" }, // reverse proxied chat off port 54321
   { title: "Browser", icon: "fa-search", url: "/scram.html" },
   { title: "Apps", icon: "fa-grid", url: "page/apps.html" },
-  { title: "Chat", icon: "fa-comments-alt", url: "page/chat.html" },
   { title: "Movies", icon: "fa-clapperboard", url: "page/mov.html" },
 ];
