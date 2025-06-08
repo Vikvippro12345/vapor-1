81a44b593a808c2317902b3e714af38cbf574ed1
technonyte dev <technonytealternate+1@outlook.com>
2025-06-08 21:11:43 +0000

    done.

--- scripts/navi.js
+++ scripts/navi.js
@@ -6,7 +6,7 @@ const navItems = [
   { title: "Home", icon: "fa-home", url: "page/home.html" },
   { title: "Games", icon: "fa-gamepad-modern", url: "page/games.html" },
   { title: "VM", icon: "fa-desktop", url: "page/vm-priv.html" },
-  { title: "Chat", icon: "fa-comments-alt", url: "/chat/" }, // reverse proxied chat off port 54321
+  { title: "Chat", icon: "fa-comments-alt", url: "/chat.html" }, // AHHGG
   { title: "Browser", icon: "fa-search", url: "/scram.html" },
   { title: "Apps", icon: "fa-grid", url: "page/apps.html" },
   { title: "Movies", icon: "fa-clapperboard", url: "page/mov.html" },
