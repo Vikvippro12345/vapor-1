654c9aad779852742295c397cd9a02b0f4ef0ee6
technonyte <technonytealternate@outlook.com>
2025-08-11 23:21:23 +0000

    EVERYTHING FOR v3.6

--- scripts/navi.js
+++ scripts/navi.js
@@ -7,14 +7,16 @@
 
 const navItems = [
   { title: "Home", icon: "fa-home", url: "page/home.html", type: "item" },
+  { type: "divider" },
   { title: "VM", icon: "fa-desktop", url: "page/vm-priv.html", type: "item" },
   { title: "Chat", icon: "fa-comments-alt", url: "/chat.html", type: "item" },
+  { title: "Tools", icon: "fa-hammer", url: "page/tools.html", type: "item" },
   { type: "divider" },
   { title: "Games", icon: "fa-gamepad", url: "page/games.html", type: "item" },
-  { title: "Emulators", icon: "fa-cube", url: "/page/apps/emu.html", type: "item" },
+  { title: "???", icon: "fa-question-circle", url: "", type: "item" },
   { title: "Browser", icon: "fa-search", url: "/scram.html", type: "item" },
   { type: "divider" },
   { title: "Movies", icon: "fa-film", url: "page/mov.html", type: "item" },
   { title: "TV Shows", icon: "fa-tv", url: "page/tv.html", type: "item" },
-  { title: "Music", icon: "fa-music", url: "page/apps/music.html", type: "item" },
+  { title: "Music", icon: "fa-music", url: "page/music.html", type: "item" },
 ];
--- scripts/particle.js
+++ scripts/particle.js
@@ -1,107 +1,80 @@
 document.addEventListener("DOMContentLoaded", function () {
   particlesJS("particles-js", {
-    particles: {
-      number: {
-        value: 100,
-        density: {
-          enable: true,
-          value_area: 800,
-        },
-      },
-      color: {
-        value: "#ffffff",
-      },
-      shape: {
-        type: "circle",
-        stroke: {
-          width: 0,
-          color: "#000000",
-        },
-        polygon: {
-          nb_sides: 5,
-        },
-      },
-      opacity: {
-        value: 0.5,
-        random: true,
-        anim: {
-          enable: false,
-          speed: 1,
-          opacity_min: 0.1,
-          sync: false,
-        },
-      },
-      size: {
-        value: 2,
-        random: true,
-        anim: {
-          enable: false,
-          speed: 40,
-          size_min: 0.1,
-          sync: false,
-        },
-      },
-      line_linked: {
+  particles: {
+    number: {
+      value: 150,
+      density: {
         enable: true,
-        distance: 50,
-        color: "#ffffff",
-        opacity: 0.1,
-        width: 1,
-      },
-      move: {
-        enable: true,
-        speed: 1.5,
-        direction: "right",
-        random: true,
-        straight: true,
-        out_mode: "out",
-        bounce: false,
-        attract: {
-          enable: false,
-          rotateX: 600,
-          rotateY: 1200,
-        },
+        value_area: 800,
       },
     },
-    interactivity: {
-      detect_on: "canvas",
-      events: {
-        onhover: {
-          enable: false,
-          mode: "repulse",
-        },
-        onclick: {
-          enable: true,
-          mode: "push",
-        },
-        resize: true,
+    color: {
+      value: "#ffffff",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
       },
-      modes: {
-        grab: {
-          distance: 400,
-          line_linked: {
-            opacity: 1,
-          },
-        },
-        bubble: {
-          distance: 400,
-          size: 40,
-          duration: 2,
-          opacity: 8,
-          speed: 3,
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4,
-        },
-        push: {
-          particles_nb: 25,
-        },
-        remove: {
-          particles_nb: 2,
-        },
+      polygon: {
+        nb_sides: 5,
       },
     },
-    retina_detect: true,
-  });
+    opacity: {
+      value: 0.5,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 1.5,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 80,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 1.5,
+      direction: "right",
+      random: true,
+      straight: true,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        particles_nb: 25,
+      },
+    },
+  },
+  retina_detect: true,
+});
 });
